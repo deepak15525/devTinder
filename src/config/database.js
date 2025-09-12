ce189d61564ea513976dc7e9c8d3648c212b2cb8
deepak15525 <92777776+deepak15525@users.noreply.github.com>
2025-09-13 01:45:07 +0530

    Change MongoDB connection string to placeholder

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
 	await mongoose.connect(
-		"mongodb+srv://Deepak:Deepak12345@cluster0.c0irzg0.mongodb.net/devTinder"
+		"xxxxxxx"
 	);
 }
 
